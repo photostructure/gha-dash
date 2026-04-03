60a38e9df212019efbbd009e05eaf6c6173b2404
Matthew McEachen <matthew@photostructure.com>
2026-04-02 17:56:52 -0700

    refactor(client): extract RefreshIcon component to DRY up reload SVG

--- src/client/components/RepoGroup.vue
+++ src/client/components/RepoGroup.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import type { RepoGroup } from "../composables/useWorkflows";
 import WorkflowRow from "./WorkflowRow.vue";
+import RefreshIcon from "./RefreshIcon.vue";
 
 const props = defineProps<{
   group: RepoGroup;
@@ -43,9 +44,7 @@ function onKeydown(e: KeyboardEvent) {
         :aria-label="`Refresh ${group.repo}`"
         @click.stop="emit('refresh', group.repo)"
       >
-        <svg width="16" height="16" viewBox="0 0 24 24" fill="currentColor" style="transform:scaleX(-1)">
-          <path d="M12 4V1L8 5l4 4V6c3.31 0 6 2.69 6 6 0 1.01-.25 1.97-.7 2.8l1.46 1.46C19.54 15.03 20 13.57 20 12c0-4.42-3.58-8-8-8zm0 14c-3.31 0-6-2.69-6-6 0-1.01.25-1.97.7-2.8L5.24 7.74C4.46 8.97 4 10.43 4 12c0 4.42 3.58 8 8 8v3l4-4-4-4v3z"/>
-        </svg>
+        <RefreshIcon />
       </button>
     </td>
   </tr>
