dc87b3cbf10c4ab7915aad9b4ac718fefae2d2a7
Matthew McEachen <matthew@photostructure.com>
2026-04-07 23:36:53 -0700

    chore(package): setup prettier

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,11 @@
 import eslint from "@eslint/js";
+import eslintConfigPrettier from "eslint-config-prettier";
 import tseslint from "typescript-eslint";
 
 export default tseslint.config(
   eslint.configs.recommended,
   ...tseslint.configs.recommended,
+  eslintConfigPrettier,
   {
     ignores: ["dist/", "node_modules/", "src/public/"],
   },
